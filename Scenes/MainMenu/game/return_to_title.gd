b45922eb31da80a41faca33215ee285ca547c778
Jonathan De Haay <jonathan.dehaay@gmail.com>
2020-11-20 09:05:30 +0900

    refactor -> move MainMenu.tscn to subfolder

--- Scenes/MainMenu/game/return_to_title.gd
+++ Scenes/MainMenu/game/return_to_title.gd
@@ -2,5 +2,5 @@ extends Control
 
 # Go back to MainMenu scene
 func _on_Button_pressed() -> void:
-	if get_tree().change_scene("res://Scenes/MainMenu.tscn") != OK:
+	if get_tree().change_scene("res://Scenes/MainMenu/MainMenu.tscn") != OK:
 		printerr("An unexpected error occured when trying to switch to the MainMenu scene")
