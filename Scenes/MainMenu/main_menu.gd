eb2ba73e0daab7eee3f9a6c4f139177edf15794e
Jonathan De Haay <jonathan.dehaay@gmail.com>
2020-11-21 14:16:41 +0900

    feat -> move script and can now quit game

--- Scenes/MainMenu/main_menu.gd
+++ Scenes/MainMenu/main_menu.gd
@@ -6,7 +6,18 @@ var scene_path_to_load: String
 func _ready() -> void:
 	$Menu/CenterRow/MenuOptions/ContinueButton.grab_focus()
 	for button in $Menu/CenterRow/MenuOptions.get_children():
-		button.connect("pressed", self, "on_button_pressed", [button.scene_to_load])
+		var props = button.get_script().get_script_property_list()
+		if array_has_property(props, "scene_to_load"):
+			button.connect("pressed", self, "on_button_pressed", [button.scene_to_load])
+
+func array_has_property(props: Array, property: String) -> bool:
+	for prop in props:
+		if prop.usage & PROPERTY_USAGE_SCRIPT_VARIABLE:
+			if prop.name == property:
+				return true
+		else:
+			return false
+	return false
 
 # Go to specified scene
 func on_button_pressed(scene_to_load: String) -> void:
@@ -14,6 +25,7 @@ func on_button_pressed(scene_to_load: String) -> void:
 	$FadeIn.show()
 	$FadeIn.fade_in()
 
+# Change scene when fade animation is over
 func _on_FadeIn_fade_finished() -> void:
 	if get_tree().change_scene(scene_path_to_load) != OK:
 		printerr("An unexpected error occured when trying to switch to the ", scene_path_to_load, " scene")
