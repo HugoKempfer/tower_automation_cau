aac8d82ffd8a55da76bcc7aa46116ad475976576
Hugo Kempfer <hugkempf@gmail.com>
2020-11-28 00:08:39 +0900

    feat -> add more accurate new construction position tracking (made with custom signals)

--- Terrain.gd
+++ Terrain.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 class TileData:
 	var is_terrain_constructible: bool
 	var construction_state := ""
@@ -22,10 +21,24 @@ func _ready() -> void:
 	for cell in cells:
 		var current_cell_tile_id: int = self.tileset_node.get_cell_item(cell.x, cell.y, cell.z)
 		tiles[cell as Vector3] = TileData.new(self.constructible_tile_ids.has(current_cell_tile_id))
-	for tile in self.tiles.values():
-		print(var2str(tile))
+	self.enable_building_mode()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func enable_building_mode() -> void:
+	var buildable = preload("res://components/constructible/buildable_tile.tscn")
+	for tile_pos in tiles.keys():
+		var tile: TileData = tiles[tile_pos]
+		if tile.is_terrain_constructible:
+			var current = buildable.instance()
+			current.pos_init($GridMap.map_to_world(tile_pos.x, tile_pos.y, tile_pos.z), tile_pos)
+			$GridMap.add_child(current)
+			current.add_to_group("available_tiles")
+			current.connect("buildable_tile_hovered", self, "handle_tile_hover")
+
+func disable_building_mode() -> void:
+	for tile in get_tree().get_nodes_in_group("buildable_tile_hovered"):
+		tile.queue_free()
+
+func handle_tile_hover(pos: Vector3) -> void:
+	print("Tile hovered, pos => ", pos)
+	$Constructions/Core.transform.origin = pos
---
+++ scripts/terrain/buildable_tile.gd
@@ -0,0 +1,19 @@
+extends Spatial
+class_name BuildableTile
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+signal buildable_tile_hovered(pos)
+# Called when the node enters the scene tree for the first time.
+
+var pos: Vector3
+var coords: Vector3
+
+func pos_init(init_pos: Vector3, coords: Vector3) -> void:
+	self.transform.origin = init_pos
+	self.coords = coords
+
+func _on_StaticBody_mouse_entered():
+	emit_signal("buildable_tile_hovered", self.transform.origin)
