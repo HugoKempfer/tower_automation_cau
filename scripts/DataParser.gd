9ee8915ffb34ede2f6f03eb87461d5560d42b2eb
Enzo Lenzi <enzolenzi@MacBook-Pro-de-Enzo.local>
2020-11-27 17:49:44 +0900

    [+] inventory system + singleton to handle file + singleton to handle inventory

---
+++ scripts/DataParser.gd
@@ -0,0 +1,24 @@
+extends Node
+
+onready var file = File.new()
+
+func load_data(url):
+	var data = {}
+
+	if url == null:
+		return
+	if !file.file_exists(url):
+		return
+	file.open(url, File.READ)
+	data = parse_json(file.get_as_text())
+	file.close()
+	return data
+
+func write_data(url, data):
+	if url == null:
+		return
+	file.open(url, File.WRITE)
+	print(data)
+	file.store_line(JSON.print(data))
+	file.close()
+	return
